05e65cb6e8f35c3632871780fc65cb81ec5ce6b4
SuJiancan <925330661@qq.com>
2017-02-09 00:58:20 +0800

    update big to nice

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
   public static void main(String[] args){
-    System.out.println("Hello, big World!");
+    System.out.println("Hello, nice World!");
     System.out.println("Hello, I like the World!");
   }
 }
